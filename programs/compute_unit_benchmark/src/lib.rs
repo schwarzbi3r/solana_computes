799fd5faaabee1ebba61b95d199bf2b1beebdaa5
Schwarzbi3r <schwarzbier@protonmail.com>
2022-03-10 12:03:51 -0500

    Update to 233 bump demo

--- programs/compute_unit_benchmark/src/lib.rs
+++ programs/compute_unit_benchmark/src/lib.rs
@@ -30,7 +30,7 @@ pub mod compute_unit_benchmark {
         msg!("Pubkey: {}", pubkey.to_string());
         solana_program::log::sol_log_compute_units();
 
-        let pda_program_id: Pubkey = pubkey!("Et36LeaAFQcyuuWWTDfDznhZ49VC4jUAJE2AForBzcuf");
+        let pda_program_id: Pubkey = pubkey!("FFmsBmAiug1hFoxgQcGXufajAv6hcv1dpJ2U5r2x8dUK");
         msg!("Bench: FindProgramAddress for close bump");
         solana_program::log::sol_log_compute_units();
         solana_program::pubkey::Pubkey::find_program_address(&[b"qgjnnkdpbjr2o2e7"],&pda_program_id);
@@ -38,7 +38,7 @@ pub mod compute_unit_benchmark {
 
         msg!("Bench: FindProgramAddress for distant bump");
         solana_program::log::sol_log_compute_units();
-        solana_program::pubkey::Pubkey::find_program_address(&[b"qgjnnkdpbjr2o2e4"],&pda_program_id);
+        solana_program::pubkey::Pubkey::find_program_address(&[b"GPPLNPqp17dRyAVZAtUebk98opJXc8ss"],&pda_program_id);
         solana_program::log::sol_log_compute_units();
 
         Ok(())
